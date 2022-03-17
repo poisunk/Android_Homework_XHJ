98f658fc75fe77f307e3c0e83368121d3a38585a
1714480752@qq.com <92618886+poisunk@users.noreply.github.com>
2022-03-17 13:20:19 +0800

    [feature] 设置下载界面的基本布局

---
+++ SecondHomework/app/src/main/java/com/example/secondhomework/page/MyRecylcerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.secondhomework.page
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *@author:created by $[poisunk]
+ *邮箱：1714480752@qq.com
+ */
+class MyRecylcerAdapter:RecyclerView.Adapter<MyRecylcerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+    }
+
+}
