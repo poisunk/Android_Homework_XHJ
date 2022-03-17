4ee93a86e26b66229e6d983af527e8aee6e77479
1714480752@qq.com <92618886+poisunk@users.noreply.github.com>
2022-03-17 21:27:47 +0800

    [feature] 增加了添加下载任务的功能

--- SecondHomework/app/src/main/java/com/example/secondhomework/page/MainActivity.kt
+++ SecondHomework/app/src/main/java/com/example/secondhomework/page/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.example.secondhomework.page
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.secondhomework.R
 import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log.d as d1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    val list:MutableList<Runnable> = ArrayList()
+    val adapter:MyRecylcerAdapter by lazy { MyRecylcerAdapter(list,this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        list_task.adapter=adapter
+        list_task.layoutManager=LinearLayoutManager(this)
+        button_add.setOnClickListener(this)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.button_add ->{
+                d1("Main","add")
+                list.add {}
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 }
--- SecondHomework/app/src/main/java/com/example/secondhomework/page/MyRecylcerAdapter.kt
+++ SecondHomework/app/src/main/java/com/example/secondhomework/page/MyRecylcerAdapter.kt
@@ -1,29 +1,34 @@
 package com.example.secondhomework.page
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.secondhomework.R
+import com.example.secondhomework.widget.MyProgressBar
 
 /**
  *@author:created by $[poisunk]
  *邮箱：1714480752@qq.com
  */
-class MyRecylcerAdapter:RecyclerView.Adapter<MyRecylcerAdapter.ViewHolder>() {
+class MyRecylcerAdapter(val list:MutableList<Runnable>,val context: Context):RecyclerView.Adapter<MyRecylcerAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_task,parent,false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.progressNum.text= (position+1).toString()
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = list.size
 
     inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
 
+        val progressBar:MyProgressBar = view.findViewById(R.id.item_progress_bar)
+
+        val progressNum:TextView = view.findViewById(R.id.item_progress_num)
     }
 
 }
